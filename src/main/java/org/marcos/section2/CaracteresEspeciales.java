e2275249f0e69e1d2e125ad4f4e2360ec96ef5c3
Marcos12UdeG <isaac.acosta@alumnos.udg.mx>
2023-03-23 11:47:45 -0600

    commit 2

---
+++ src/main/java/org/marcos/section2/CaracteresEspeciales.java
@@ -0,0 +1,11 @@
+package org.marcos.section2;
+
+public class CaracteresEspeciales {
+    public static void main(String[] args) {
+        char espacio = '\u0020';
+        char retroceso = '\b';
+        char tabular = '\t';
+        char saltoLinea = '\n';
+        System.out.println("espacio ="+ System.lineSeparator()+"2");
+    }
+}
