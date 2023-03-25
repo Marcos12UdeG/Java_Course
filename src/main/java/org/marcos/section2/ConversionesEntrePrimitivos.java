dc27c7aa31e681dbbbef289acbc161f099d6e3d9
Marcos12UdeG <isaac.acosta@alumnos.udg.mx>
2023-03-25 14:15:52 -0600

    commit new

---
+++ src/main/java/org/marcos/section2/ConversionesEntrePrimitivos.java
@@ -0,0 +1,15 @@
+package org.marcos.section2;
+
+public class ConversionesEntrePrimitivos {
+    public static void main(String[] args) {
+
+        int i = 1000;
+
+        long num = (long)i;
+        byte numero = (byte)i;
+
+        System.out.println("numero = " + numero);
+
+        System.out.println("num = " + num);
+    }
+}
