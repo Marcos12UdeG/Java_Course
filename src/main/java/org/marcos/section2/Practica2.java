ba6f986eafebc68be529221860160110f6326a67
Marcos12UdeG <isaac.acosta@alumnos.udg.mx>
2023-03-25 14:52:01 -0600

    commit Practicanew

---
+++ src/main/java/org/marcos/section2/Practica2.java
@@ -0,0 +1,80 @@
+package org.marcos.section2;
+
+import java.util.Scanner;
+
+public class Practica2 {
+
+
+    public static void main(String[] args)
+    {
+        Scanner entrada = new Scanner(System.in);
+        int op;
+
+        String nombre="";
+        int folio=0;
+        String dueno ="";
+        String direccion="";
+
+
+        do {
+            System.out.println("MENU");
+            System.out.println("1.- CAPTURAR");
+            System.out.println("2.- MOSTRAR FACTURA");
+            System.out.println("3.- BUSCAR FACTURA POR NOMBRE");
+            op = entrada.nextInt();
+
+            switch(op)
+            {
+                case 1:
+                {
+                    System.out.println("INGRESAR NOMBRE");
+                    nombre = entrada.next();
+                    System.out.println("INGRESAR NOMBRE DEL DUENO");
+                    dueno = entrada.next();
+                    System.out.println("INGRESAR DIRECCION");
+                    direccion = entrada.next();
+                    System.out.println("INGRESAR FOLIO");
+                    folio = entrada.nextInt();
+                    break;
+                }
+                case 2:
+                {
+                    System.out.println("nombre = " + nombre);
+                    System.out.println("dueno = " + dueno);
+                    System.out.println("folio = " + folio);
+                    System.out.println("direccion = " + direccion);
+                    break;
+                }
+
+                case 3:
+                {
+                    String NB = "";
+                    System.out.println("DAME EL NOMBRE A BUSCAR");
+                    NB = entrada.next();
+
+                    int value = nombre.compareTo(NB);
+
+                    if(value == 0)
+                    {
+                        System.out.println("SE ENCONTRO EL REGISTRO");
+                        System.out.println("nombre = " + nombre);
+                        System.out.println("dueno = " + dueno);
+                        System.out.println("folio = " + folio);
+                        System.out.println("direccion = " + direccion);
+
+                    }
+
+                    else
+                    {
+                        System.out.println("NO SE ENCONTRO EL REGISTRO");
+                    }
+                    break;
+
+                }
+
+            }
+
+        }while(op !=4);
+
+    }
+}
