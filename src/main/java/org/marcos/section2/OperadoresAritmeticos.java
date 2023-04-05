e66808fc35d6eab59cd51b5db4d7e5c0802960e5
Marcos12UdeG <isaac.acosta@alumnos.udg.mx>
2023-04-05 14:05:30 -0600

    commit video34

---
+++ src/main/java/org/marcos/section2/OperadoresAritmeticos.java
@@ -0,0 +1,15 @@
+package org.marcos.section2;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+       int i =5 , j=4;
+
+        System.out.println("(i+j) = " + (i+j));
+        System.out.println("(i-j) = " + (i-j));
+        System.out.println("(i*j) = " + (i*j));
+        System.out.println("(i/j) = " + (i/j));
+    }
+    
+
+
+}
