f8ebb8e0f914f02faea0b32683586a6d0b5f1e1c
Marcos12UdeG <isaac.acosta@alumnos.udg.mx>
2023-04-05 00:31:19 -0600

    commit video33

---
+++ src/main/java/org/marcos/section2/ExtensionDeUnArchivo.java
@@ -0,0 +1,11 @@
+package org.marcos.section2;
+
+public class ExtensionDeUnArchivo {
+    public static void main(String[] args) {
+      String extension = "algo.jpeg";
+        int i = extension.indexOf(".");//retorna la posicion del char;
+        System.out.println("extension.length() = " + extension.length());
+        System.out.println("extension.substring(extension.length()-3) = " + extension.substring(extension.length()-3));
+        System.out.println("extension = " + extension.substring(i+1));//extrae caracteres;
+    }
+}
